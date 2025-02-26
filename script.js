fc1180a49a91404907a20af60ddd69ea1bf89259
Wikire <saymonaraujo36@gmail.com>
2025-02-26 17:50:03 -0300

    atualização-5

---
+++ script.js
@@ -0,0 +1,11 @@
+function toggle() {
+  const html = document.documentElement;
+  html.classList.toggle("light");
+
+  const img = document.querySelector("#person img");
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/quikire2.jpg");
+  } else {
+    img.setAttribute("src", "./assets/quikire.jpg");
+  }
+}
